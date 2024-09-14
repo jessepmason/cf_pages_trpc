c64257a31430349ef09b86b38c69420d80493420
Jesse <jessepmason@gmail.com>
2024-09-14 16:34:47 -0400

    trying worker.js file in root

---
+++ _worker.js
@@ -0,0 +1,9 @@
+export default {
+    async fetch(request, env) {
+        const url = new URL(request.url);
+        return new Response('Hello');
+        //  Otherwise, serve the static assets.
+        // Without this, the Worker will error andno assets will be served.
+        // return env.ASSETS.fetch(request);
+    },
+  }
