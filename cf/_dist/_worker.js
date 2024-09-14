dad44e30ba19a9d401d6d0d3ad392329496e46dd
Jesse <jessepmason@gmail.com>
2024-09-14 19:37:49 -0400

    added esbuild

--- cf/_dist/_worker.js
+++ cf/_dist/_worker.js
@@ -1,9 +1,9 @@
-export default {
+(() => {
+  // src/worker.js
+  var worker_default = {
     async fetch(request, env) {
-        const url = new URL(request.url);
-        return new Response('Hello From Dist');
-        //  Otherwise, serve the static assets.
-        // Without this, the Worker will error andno assets will be served.
-        // return env.ASSETS.fetch(request);
-    },
-}
+      const url = new URL(request.url);
+      return new Response("Hello From Dist");
+    }
+  };
+})();
