e39f5471352c013ae03e02d7151c7f37700ea1dd
Jesse <jessepmason@gmail.com>
2024-09-14 21:01:08 -0400

    import test

--- cf/_dist/_worker.js
+++ cf/_dist/_worker.js
@@ -1,7 +1,9 @@
+import * as Test from "./file_1"
+
 export default {
   async fetch(request, env) {
       const url = new URL(request.url);
-      return new Response('Hello From Dist');
+      return new Response(Test.test());
       //  Otherwise, serve the static assets.
       // Without this, the Worker will error andno assets will be served.
       // return env.ASSETS.fetch(request);
--- cf/src/worker.ts
+++ cf/src/worker.ts
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
-        return new Response('Hello From Dist');
+        return new Response('Hello From Dist TS');
         //  Otherwise, serve the static assets.
         // Without this, the Worker will error andno assets will be served.
         // return env.ASSETS.fetch(request);
