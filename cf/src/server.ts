478f549fa283d6f56f5553d71871daa59b1e08d7
Jesse <jessepmason@gmail.com>
2024-09-15 21:00:11 -0400

    adding trpc

--- cf/src/server.ts
+++ cf/src/server.ts
@@ -1,7 +1,25 @@
+
+
+import {fetchRequestHandler} from "@trpc/server/adapters/fetch"
+import { appRouter } from "./trpc_router";
 export default {
-    async fetch(request, env) {
+    async fetch(request, env, context) {
         const url = new URL(request.url);
-        return new Response('Hello From Dist TS');
+        if(request.path == "/"){
+            return new Response('Hello FHome');
+        }
+        const trpcResults = await fetchRequestHandler({
+            endpoint:"/api",
+            req:context.request,
+            router:appRouter,
+            createContext: ()=>({})
+        })
+        if(trpcResults.ok){
+            return new Response('Hello TRPC');
+
+        }else{
+            return new Response("Not found")
+        }
         //  Otherwise, serve the static assets.
         // Without this, the Worker will error andno assets will be served.
         // return env.ASSETS.fetch(request);
