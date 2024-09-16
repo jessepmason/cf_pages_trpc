bb101f42bb0b3be52fc67c9a60aa43c81cc0f8f3
Jesse <jessepmason@gmail.com>
2024-09-16 12:09:44 -0400

    started adding client

--- cf/src/server.ts
+++ cf/src/server.ts
@@ -4,11 +4,34 @@ import {fetchRequestHandler} from "@trpc/server/adapters/fetch"
 import { appRouter } from "./trpc_router";
 
 export default {
-    async fetch(request :Request, env, context) {
+    async fetch(request :Request, env, ctx) {
         const url = new URL(request.url);
+
         if(url.pathname == "/"){
+
             return new Response('Hello FHome');
+
+        }else if (url.pathname.startsWith("/public")){
+                
+            return await ctx.env.ASSETS.fetch(ctx.req.raw);
+
+        }else if (url.pathname == "/react"){
+            const html =  `<!DOCTYPE html>
+                        <html>
+                        <body>
+
+                        <h1>My First Heading</h1>
+                        <p>My first paragraph.</p>
+
+                        </body>
+                    </html>`
+            return new Response(html, {
+                headers: {
+                    "content-type": "text/html;charset=UTF-8",
+                },
+            });
         }
+
         const trpcResults = await fetchRequestHandler({
             endpoint:"/api",
             req:request,
@@ -17,14 +40,12 @@ export default {
 
             })
         })
-        if(trpcResults.ok){
-            return new Response('Hello TRPC');
 
+        if(trpcResults.ok){
+            return trpcResults
         }else{
             return new Response("Not found")
         }
-        //  Otherwise, serve the static assets.
-        // Without this, the Worker will error andno assets will be served.
-        // return env.ASSETS.fetch(request);
+
     },
 }
